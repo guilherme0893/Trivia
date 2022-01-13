5e7f7f38126e1aa31e2b0a56c3d43084163a6b02
LuisSoares <jobluisfillipe@gmail.com>
2022-01-13 18:13:38 -0300

    [Grupo 27] Requisito 3

--- src/Routes.js
+++ src/Routes.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router';
 import Login from './components/Login';
+import Settings from './components/Settings';
 
 export default class App extends Component {
   render() {
     return (
       <Switch>
         <Route exact path="/" component={ Login } />
+        <Route exact path="/settings" component={ Settings } />
       </Switch>
     );
   }
